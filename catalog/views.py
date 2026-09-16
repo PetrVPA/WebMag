41255e0e322d368cd0b7c3e61a19793b3cc3214e
PetrVPA <vpa.interceptor@gmail.com>
2026-09-16 16:49:56 +0300

    Второе сохранение

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib import messages
-from django.shortcuts import redirect
+
 
 def home(request):
     return render(request, 'home.html')
