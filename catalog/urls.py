a11d004eb517ebb1ebb516fc9c47e48e3bbb8b6a
PetrVPA <vpa.interceptor@gmail.com>
2026-09-17 13:28:22 +0300

    Четвертое сохранение

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,10 @@
 
 from django.urls import path
-#from catalog.apps import CatalogConfig
-from catalog.views import home
-from catalog.views import contacts
+from . import views
 
-
-#app_name = CatalogConfig.name
-app_name = 'catalog'
 
 urlpatterns = [
-    path('home/', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-
+    path("", views.home),
+    path("home/", views.home),
+    path("contacts/", views.contacts),
 ]
--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('', include('catalog.urls', namespace='catalog')),
-    path('contacts/', include('catalog.urls', namespace='catalog')),
-    path('admin/', admin.site.urls),
 
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include('catalog.urls')),
 ]
